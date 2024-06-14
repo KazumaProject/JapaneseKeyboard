8a1dde5d7e312f85a66493cc8b3de9e92169cea0
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-06-14 13:36:51 -0400

    Update SuggestionAdapter.kt

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/ime_service/adapters/SuggestionAdapter.kt
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/ime_service/adapters/SuggestionAdapter.kt
@@ -60,12 +60,13 @@ class SuggestionAdapter : RecyclerView.Adapter<SuggestionAdapter.SuggestionViewH
             val typeText = findViewById<MaterialTextView>(R.id.suggestion_item_type_text_view)
             text.text = suggestion.string
             typeText.text = when(suggestion.type){
-                (1).toByte() -> "   "
-                (2).toByte() -> "[部]"
-                (3).toByte() -> "[ひらがな]"
-                (4).toByte() -> "[カタカナ]"
-                (5).toByte() -> "[候補]"
-                (6).toByte() -> "[単]"
+                (1).toByte() -> ""
+                (2).toByte() -> "部"
+                (3).toByte() -> "ひらがな"
+                (4).toByte() -> "カタカナ"
+                (5).toByte() -> ""
+                (6).toByte() -> "候補"
+                (7).toByte() -> "単"
 //                (1).toByte() -> "[N-Best][${suggestion.leftId}][${suggestion.rightId}][${suggestion.score}]"
 //                (2).toByte() -> "[部][${suggestion.leftId}][${suggestion.rightId}][${suggestion.score}]"
 //                (3).toByte() -> "[ひらがな][${suggestion.score}]"
