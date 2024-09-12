a26213fd432d8dfa513a839abf17ddb0474f7613
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-09-12 05:36:17 -0400

    fixed text size on keys

--- tenkey/src/main/java/com/kazumaproject/tenkey/extensions/AppCompatButtonExtension.kt
+++ tenkey/src/main/java/com/kazumaproject/tenkey/extensions/AppCompatButtonExtension.kt
@@ -7,9 +7,9 @@ import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContextCompat
 import com.kazumaproject.tenkey.R
 
-const val KEY_JAPANESE_SIZE = 20f
+const val KEY_JAPANESE_SIZE = 18f
 const val KEY_ENGLISH_SIZE = 16f
-const val KEY_NUMBER_SIZE = 20f
+const val KEY_NUMBER_SIZE = 18f
 
 private val JP_KEY_LAYOUT_WITH_SPACE = listOf(
     "    あ    ", "    か    ", "    さ    ", "    た    ", "    な    ",
