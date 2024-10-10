83e5e84762ea65effd6570e0c363ea6ff009caee
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-10-10 04:35:47 -0400

    日本語、英語、数字を切り替えるボタンのデザインの変更

--- tenkey/src/main/java/com/kazumaproject/tenkey/InputModeSwitch.kt
+++ tenkey/src/main/java/com/kazumaproject/tenkey/InputModeSwitch.kt
@@ -1,121 +1,44 @@
 package com.kazumaproject.tenkey
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Typeface
 import android.util.AttributeSet
 import androidx.core.content.ContextCompat
-import com.kazumaproject.tenkey.extensions.convertDp2PxFloat
-import com.kazumaproject.tenkey.extensions.convertSp2Px
 import com.kazumaproject.tenkey.state.InputMode
 
-class InputModeSwitch(context: Context, attrs: AttributeSet): androidx.appcompat.widget.AppCompatImageButton(context, attrs) {
-
-    private val textPaint = Paint().apply {
-        color = ContextCompat.getColor(context, R.color.sub_text_color)
-        textSize = 16f.convertSp2Px(context)
-        textAlign = Paint.Align.CENTER
-    }
-
-    private val textPaintBold = Paint().apply {
-        color = ContextCompat.getColor(context, R.color.keyboard_enter_key_color)
-        textSize = 16f.convertSp2Px(context)
-        textAlign = Paint.Align.CENTER
-        typeface = Typeface.create(Typeface.DEFAULT, Typeface.BOLD)
-    }
+class InputModeSwitch(context: Context, attrs: AttributeSet) :
+    androidx.appcompat.widget.AppCompatImageButton(context, attrs) {
 
     private var currentInputMode: InputMode = InputMode.ModeJapanese
 
-    fun setInputMode(inputMode: InputMode){
+    fun setInputMode(inputMode: InputMode) {
         currentInputMode = inputMode
-        this.invalidate()
+        when (inputMode) {
+            InputMode.ModeJapanese -> setImageDrawable(
+                ContextCompat.getDrawable(
+                    context,
+                    R.drawable.input_mode_japanese_select
+                )
+            )
+
+            InputMode.ModeEnglish -> setImageDrawable(
+                ContextCompat.getDrawable(
+                    context,
+                    R.drawable.input_mode_english_select
+                )
+            )
+
+            InputMode.ModeNumber -> setImageDrawable(
+                ContextCompat.getDrawable(
+                    context,
+                    R.drawable.input_mode_number_selected
+                )
+            )
+        }
     }
 
     fun getCurrentInputMode(): InputMode {
         return currentInputMode
     }
-    override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
-        val text = "あa1"
-        when(currentInputMode){
-            is InputMode.ModeJapanese ->{
-                canvas.apply {
-                    drawText(
-                        text,
-                        0,
-                        1,
-                        (width / 2f) - 12f.convertDp2PxFloat(context),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaintBold
-                    )
-                    drawText(
-                        text,
-                        1,
-                        3,
-                        ((width / 2f) + 12f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaint)
-                }
-            }
-            is InputMode.ModeEnglish ->{
-                canvas.apply {
-                    drawText(
-                        text,
-                        0,
-                        1,
-                        ((width / 2f) - 12f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaint
-                    )
-                    drawText(
-                        text,
-                        1,
-                        2,
-                        ((width / 2f) + 6f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaintBold
-                    )
-                    drawText(
-                        text,
-                        2,
-                        3,
-                        ((width / 2f) + 18f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaint
-                    )
-                }
-            }
-            is InputMode.ModeNumber ->{
-                canvas.apply {
-                    drawText(
-                        text,
-                        0,
-                        1,
-                        ((width / 2f) - 12f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaint)
-                    drawText(
-                        text,
-                        1,
-                        2,
-                        ((width / 2f) + 6f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaint
-                    )
-                    drawText(
-                        text,
-                        2,
-                        3,
-                        ((width / 2f) + 18f.convertDp2PxFloat(context)),
-                        (height / 2f) + 8f.convertDp2PxFloat(context),
-                        textPaintBold
-                    )
-                }
-            }
-        }
-    }
-
 
 
 }
