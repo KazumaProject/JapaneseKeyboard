d5452252a2500ee39f9d793239dfe8376f4c85b7
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-10-24 09:53:01 -0400

    adjust text color for symbol items

--- tenkey/src/main/java/com/kazumaproject/tenkey/extensions/AppCompatButtonExtension.kt
+++ tenkey/src/main/java/com/kazumaproject/tenkey/extensions/AppCompatButtonExtension.kt
@@ -8,7 +8,7 @@ import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.content.ContextCompat
 import com.kazumaproject.tenkey.R
 
-const val KEY_JAPANESE_SIZE = 19f
+const val KEY_JAPANESE_SIZE = 17f
 const val KEY_ENGLISH_SIZE = 15f
 const val KEY_NUMBER_SIZE = 18f
 
