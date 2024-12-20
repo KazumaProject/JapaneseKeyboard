39e52864fee450034b6e87bca8ab29e1555439c8
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2024-12-20 10:44:34 -0500

    implement fragment to show learn dictionary

---
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/dictionary_learn/DictionaryLearnFragment.kt
@@ -0,0 +1,26 @@
+package com.kazumaproject.markdownhelperkeyboard.setting_activity.ui.dictionary_learn
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DictionaryLearnFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
