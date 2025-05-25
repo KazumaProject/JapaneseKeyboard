a93705d2588506ad0f5fed3dd5eb196d8e29163d
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2025-05-25 15:21:34 -0400

    move state and key to core from tenkey

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/ime_service/tablet/TabletKeyRect.kt
+++
@@ -1,9 +0,0 @@
-package com.kazumaproject.markdownhelperkeyboard.ime_service.tablet
-
-data class TabletKeyRect(
-    val key: TabletKey,
-    val left: Int,
-    val top: Int,
-    val right: Int,
-    val bottom: Int
-)
--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/ime_service/tablet/TabletPressedKey.kt
+++
@@ -1,8 +0,0 @@
-package com.kazumaproject.markdownhelperkeyboard.ime_service.tablet
-
-data class TabletPressedKey(
-    var tabletKey: TabletKey,
-    var pointer: Int,
-    var initialX: Float,
-    var initialY: Float,
-)
