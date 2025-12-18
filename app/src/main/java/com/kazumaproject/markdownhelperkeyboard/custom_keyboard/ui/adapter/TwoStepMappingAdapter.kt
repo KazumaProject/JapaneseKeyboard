9b7fc714027edb031db864adff6e48cbdb45e5a1
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2025-12-18 18:08:31 -0500

    implement two step flick in custom keyboard

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/custom_keyboard/ui/adapter/TwoStepMappingAdapter.kt
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/custom_keyboard/ui/adapter/TwoStepMappingAdapter.kt
@@ -15,11 +15,17 @@ class TwoStepMappingAdapter(
 
     companion object {
         private val DIFF = object : DiffUtil.ItemCallback<TwoStepMappingItem>() {
-            override fun areItemsTheSame(oldItem: TwoStepMappingItem, newItem: TwoStepMappingItem): Boolean {
+            override fun areItemsTheSame(
+                oldItem: TwoStepMappingItem,
+                newItem: TwoStepMappingItem
+            ): Boolean {
                 return oldItem.id == newItem.id
             }
 
-            override fun areContentsTheSame(oldItem: TwoStepMappingItem, newItem: TwoStepMappingItem): Boolean {
+            override fun areContentsTheSame(
+                oldItem: TwoStepMappingItem,
+                newItem: TwoStepMappingItem
+            ): Boolean {
                 return oldItem == newItem
             }
         }
@@ -39,7 +45,81 @@ class TwoStepMappingAdapter(
     override fun onBindViewHolder(holder: VH, position: Int) {
         val item = getItem(position)
 
-        holder.binding.textDirections.text = "${item.first.name} → ${item.second.name}"
+        val displayText = when {
+            item.first.name == "TAP" && item.second.name == "TAP" -> {
+                "タップ"
+            }
+
+            item.first.name == "UP_LEFT" && item.second.name == "UP_LEFT" -> {
+                "左上フリック"
+            }
+
+            item.first.name == "DOWN_LEFT" && item.second.name == "DOWN_LEFT" -> {
+                "左下フリック"
+            }
+
+            item.first.name == "UP_RIGHT" && item.second.name == "UP_RIGHT" -> {
+                "右上フリック"
+            }
+
+            item.first.name == "DOWN_RIGHT" && item.second.name == "DOWN_RIGHT" -> {
+                "右下フリック"
+            }
+
+            item.first.name == "LEFT" && item.second.name == "LEFT" -> {
+                "左フリック"
+            }
+
+            item.first.name == "LEFT" && item.second.name == "DOWN_LEFT" -> {
+                "左 + 下フリック"
+            }
+
+            item.first.name == "LEFT" && item.second.name == "UP_LEFT" -> {
+                "左 + 上フリック"
+            }
+
+            item.first.name == "RIGHT" && item.second.name == "RIGHT" -> {
+                "右フリック"
+            }
+
+            item.first.name == "RIGHT" && item.second.name == "DOWN_RIGHT" -> {
+                "右 + 下フリック"
+            }
+
+            item.first.name == "RIGHT" && item.second.name == "UP_RIGHT" -> {
+                "右 + 上フリック"
+            }
+
+            item.first.name == "UP" && item.second.name == "UP" -> {
+                "上フリック"
+            }
+
+            item.first.name == "UP" && item.second.name == "UP_LEFT" -> {
+                "上 + 右フリック"
+            }
+
+            item.first.name == "UP" && item.second.name == "UP_RIGHT" -> {
+                "上 + 左フリック"
+            }
+
+            item.first.name == "DOWN" && item.second.name == "DOWN" -> {
+                "下フリック"
+            }
+
+            item.first.name == "DOWN" && item.second.name == "DOWN_LEFT" -> {
+                "下 + 右フリック"
+            }
+
+            item.first.name == "DOWN" && item.second.name == "DOWN_RIGHT" -> {
+                "下 + 左フリック"
+            }
+
+            else -> {
+                "タップ"
+            }
+        }
+
+        holder.binding.textDirections.text = displayText
         holder.binding.editOutput.setText(item.output)
 
         holder.binding.editOutput.doAfterTextChanged { editable ->
