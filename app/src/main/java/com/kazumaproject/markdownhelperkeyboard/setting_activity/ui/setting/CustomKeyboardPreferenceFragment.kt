7763c47399d0022d1acac84484298d070b9479b8
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-03-18 22:19:19 -0400

    add preference to move fragment to change letter and key sizein custom keyboard

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/CustomKeyboardPreferenceFragment.kt
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/CustomKeyboardPreferenceFragment.kt
@@ -1,6 +1,8 @@
 package com.kazumaproject.markdownhelperkeyboard.setting_activity.ui.setting
 
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.kazumaproject.markdownhelperkeyboard.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,5 +12,15 @@ class CustomKeyboardPreferenceFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_custom, rootKey)
+
+        val customKeyboardSizePreference =
+            findPreference<Preference>("custom_keyboard_size_preference")
+
+        customKeyboardSizePreference?.apply {
+            setOnPreferenceClickListener {
+                findNavController().navigate(R.id.action_navigation_setting_to_flickKeyboardSizeSettingsFragment)
+                true
+            }
+        }
     }
 }
