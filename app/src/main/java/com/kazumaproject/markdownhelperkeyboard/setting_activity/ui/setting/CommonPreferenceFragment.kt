16a1ac85353db9bb7c8fc6f0de9382b7ea686b24
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-04-15 19:21:27 -0400

    sync emoji color in pref_common

--- app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/CommonPreferenceFragment.kt
+++ app/src/main/java/com/kazumaproject/markdownhelperkeyboard/setting_activity/ui/setting/CommonPreferenceFragment.kt
@@ -453,6 +453,7 @@ class CommonPreferenceFragment : PreferenceFragmentCompat() {
         findPreference<ListPreference>("default_emoji_skin_tone_preference")?.apply {
             summaryProvider = ListPreference.SimpleSummaryProvider.getInstance()
         }
+        syncDefaultEmojiSkinTonePreference()
 
         findPreference<SeekBarPreference>("flick_sensitivity_preference")?.apply {
             summary = when (this.value) {
@@ -550,6 +551,11 @@ class CommonPreferenceFragment : PreferenceFragmentCompat() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        syncDefaultEmojiSkinTonePreference()
+    }
+
     // リーク対策: RecyclerViewの参照を断ち切る
     override fun onDestroyView() {
         try {
@@ -558,6 +564,15 @@ class CommonPreferenceFragment : PreferenceFragmentCompat() {
             // Viewが生成されていない場合などを考慮して例外は無視
         }
         super.onDestroyView()
+    }
+
+    private fun syncDefaultEmojiSkinTonePreference() {
+        findPreference<ListPreference>("default_emoji_skin_tone_preference")?.apply {
+            val savedSkinTone = appPreference.default_emoji_skin_tone_preference
+            if (value != savedSkinTone) {
+                value = savedSkinTone
+            }
+        }
     }
 
     @SuppressLint("CheckResult")
